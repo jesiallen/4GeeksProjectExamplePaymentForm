1ed604e62128233ea0ba9f619fad863216dcf7a5
jesiallen <justjesi2010@gmail.com>
2022-03-26 17:53:12 +0000

    Completed project

--- src/app.js
+++ src/app.js
@@ -32,9 +32,37 @@ let validate = event => {
     addError("Invalid card number.");
     cardNumber.classList.add("is-invalid");
   }
-  if (cvcNumber.value.length != 16) {
-    addError("Invalid card number.");
-    cardNumber.classList.add("is-invalid");
+  if (cvcNumber.value.length != 3) {
+    addError("Invalid CVC number.");
+    cvcNumber.classList.add("is-invalid");
+  }
+  if (purchaseAmount.value.length != 4) {
+    addError("Incorrect puchase amount.");
+    purchaseAmount.classList.add("is-invalid");
+  }
+  if (firstName.value.length < 2) {
+    addError("Please enter first name.");
+    firstName.classList.add("is-invalid");
+  }
+  if (lastName.value.length < 2) {
+    addError("Please enter last name.");
+    lastName.classList.add("is-invalid");
+  }
+  if (inputCity.value.length < 2) {
+    addError("Please enter your city.");
+    inputCity.classList.add("is-invalid");
+  }
+  if (inputState.value.length != 2) {
+    addError("Please choose your state.");
+    inputState.classList.add("is-invalid");
+  }
+  if (inputZip.value.length != 5) {
+    addError("Please enter 5 digit zip code.");
+    inputZip.classList.add("is-invalid");
+  }
+  if (message.value.length > 50) {
+    addError("Message is limited to 50 characters.");
+    message.classList.add("is-invalid");
   }
 };
 
